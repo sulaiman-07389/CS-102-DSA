961632ab80d70f70691afaf708c76fe2199d9835
sulaiman-07389 <97950626+sulaiman-07389@users.noreply.github.com>
2022-02-08 00:36:17 +0500

    Create BalancedBracketsCodeStack.py

---
+++ BalancedBracketsCodeStack.py
@@ -0,0 +1,22 @@
+s=input()
+TypesOfBraces = {'(': ')','{': '}','[': ']'}
+
+def balanced_braces(s):
+    stack = []
+    for i in s:
+        if i in TypesOfBraces:
+            stack.append(i)
+            continue
+        try:
+            OpeningBrace = stack.pop()
+        except IndexError:  
+            return False
+        if not(i == TypesOfBraces[OpeningBrace]):  
+            return False
+    if len(stack) == 0:
+        return True
+    else:
+        return False
+        
+
+print(balanced_braces(s))
